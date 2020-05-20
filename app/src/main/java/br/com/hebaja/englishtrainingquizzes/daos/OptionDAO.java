f78b1afa891ec25f1333577f6725e5f44c5974cd
Henrique Jarbas <hebaja@yahoo.com.br>
2020-05-20 16:22:32 -0300

    More question objects added

--- app/src/main/java/br/com/hebaja/englishtrainingquizzes/daos/OptionDAO.java
+++ app/src/main/java/br/com/hebaja/englishtrainingquizzes/daos/OptionDAO.java
@@ -14,7 +14,11 @@ public class OptionDAO {
         List<Option> options = new ArrayList<>(Arrays.asList(
             new Option("Phrasal Verbs", "phrasal_verbs_questions.json", order++),
             new Option("Preposition", "prepositions_questions.json", order++),
-            new Option("Modals", "modal_verbs_questions.json", order++)));
+            new Option("Modals", "modal_verbs_questions.json", order++),
+            new Option("Active and Passive voice", "active_to_passive.json", order++),
+            new Option("Comparative and Superlative", "comparative_superlative.json", order++),
+            new Option("Reported Speech", "reported_speech.json", order++),
+            new Option("Noun Clauses", "questions_format.json", order++)));
         return options;
     }
 }
